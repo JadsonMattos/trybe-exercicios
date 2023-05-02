1ef9ba6040c1cab67e0f2497cb20e131ebf33f62
JadsonMattos <jadson.sousamattos@gmail.com>
2023-05-02 12:35:28 -0300

    04 - Retornando valores positivos, negativos ou zero

--- fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/exercicios-agora-a-pratica/script.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/exercicios-agora-a-pratica/script.js
@@ -42,3 +42,17 @@ else if (f > e && f > g) {
 else {
     console.log('Eu sou a letra (g) com o maior valor: ' + g);
 }
+
+// 4 - Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne: “positive”, se esse valor for positivo; “negative”, se esse valor for negativo, e “zero”, caso esse valor não seja nem positivo nem negativo.
+
+let h = 82;
+
+if (h > 0) {
+    console.log('positive');
+}
+else if (h < 0) {
+    console.log('negative');
+}
+else {
+    console.log('zero');
+}
