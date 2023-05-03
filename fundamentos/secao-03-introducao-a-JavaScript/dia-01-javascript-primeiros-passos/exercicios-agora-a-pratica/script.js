687f083e5844d9e086bb5d8cbba55588b7863e69
JadsonMattos <jadson.sousamattos@gmail.com>
2023-05-03 07:25:01 -0300

    10 e 11 - if e else com problemas mais complexos

--- fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/exercicios-agora-a-pratica/script.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/exercicios-agora-a-pratica/script.js
@@ -186,3 +186,105 @@ if (l % 2 !== 0 || m % 2 !== 0 || n % 2 !== 0) {
 else {
     console.log(true);
 }
+
+// 10 - Utilize if/else para escrever um código que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, o código deve calcular o lucro (ou seja, o valor de venda menos o custo do produto) da empresa ao vender mil produtos.
+
+// Atente para o fato de que um imposto de 20% incide sobre o custo do produto.
+
+// Seu código deve emitir uma mensagem de erro e encerrar caso algum dos valores de entrada seja menor que zero.
+
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo e deve considerar que o imposto de 20% faz parte do valor de custo.
+
+// valorCustoTotal = valorCusto + impostoSobreOCusto;
+// lucro = valorVenda - valorCustoTotal (lucro de um produto);
+
+let buyValue = 1;
+let sellValue = 3;
+let total;
+
+if (buyValue < 0 || sellValue < 0) {
+    console.log('Inválido!');
+}
+else {
+    total = ((sellValue - (buyValue * 0.2) + buyValue) * 1000);
+}
+console.log(total);
+
+// 11 - Utilize if/else para escrever um código que, dado um salário bruto, calcule o salário líquido a ser recebido.
+
+// Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR.
+
+// A notação para um salário de R$1.500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+
+// INSS (Instituto Nacional do Seguro Social)
+
+// Salário bruto até R$ 1.556,94: alíquota de 8%
+
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+
+// IR (Imposto de Renda)
+
+// Até R$ 1.903,98: isento de imposto de renda
+
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+
+// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto
+
+// Exemplo: Uma pessoa que possui o salário bruto de R$ 3.000,00 terá o primeiro desconto referente à contribuição do INSS. O cálculo deve ser o demonstrado a seguir.
+
+// O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para o INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
+
+// Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
+
+// Para calcular o valor do IR, considera-se um salário-base (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, em que a alíquota é de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, tem-se:
+
+// R$ 2.670,00 - salário com INSS já deduzido.
+// 7.5% - alíquota de imposto de renda, que representa um desconto de R$ 200,25.
+// R$ 142,80 - parcela a ser deduzida do imposto de renda.
+// Para obter o valor do imposto de renda, calcula-se: R$ 200,25 (que representa 7,5% de R$ 2.670,00) - R$ 142,80 (dedução do imposto de renda) = R$ 57,45.
+
+// Para obter o salário líquido, calcula-se: R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+
+// Resultado: R$ 2.612,55.
+
+let salarioBruto = 3000.00;
+let liquidoInss;
+let liquidoIr;
+
+if (salarioBruto <= 1556.94) {
+    liquidoInss = salarioBruto - (salarioBruto * 0.08);
+}
+else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    liquidoInss = salarioBruto - (salarioBruto * 0.09);
+}
+else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    liquidoInss = salarioBruto - (salarioBruto * 0.11);
+}
+else {
+    liquidoInss = salarioBruto - 570.88;
+}
+
+if (liquidoInss <= 1903.98) {
+    liquidoIr = liquidoInss;
+}
+else if (liquidoInss >= 1903.99 && liquidoInss <= 2826.65) {
+    liquidoIr = liquidoInss - ((liquidoInss * 0.075) - 142.80);
+}
+else if (liquidoInss >= 2826.66 && liquidoInss <= 3751.05) {
+    liquidoIr = liquidoInss - ((liquidoInss * 0.15) - 354.80);
+}
+else if (liquidoInss >= 3751.06 && liquidoInss <= 4664.68) {
+    liquidoIr = liquidoInss - ((liquidoInss * 0.225) - 636.13);
+}
+else {
+    liquidoIr = liquidoInss - ((liquidoInss * 0.275) - 869.36);
+}
+console.log(liquidoIr);
