97fe9fad79b0d6ef637fedd81245d164c78f957e
JadsonMattos <jadson.sousamattos@gmail.com>
2023-05-03 01:17:33 -0300

    09 - Retornando ímpar

--- fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/exercicios-agora-a-pratica/script.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/exercicios-agora-a-pratica/script.js
@@ -173,3 +173,16 @@ if (i % 2 === 0 || j % 2 === 0 || k % 2 === 0) {
 else {
     console.log(false);
 }
+
+// 9 - Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, o código deve retornar false.
+
+let l = 12;
+let m = 14;
+let n = 16;
+
+if (l % 2 !== 0 || m % 2 !== 0 || n % 2 !== 0) {
+    console.log(false);
+}
+else {
+    console.log(true);
+}
