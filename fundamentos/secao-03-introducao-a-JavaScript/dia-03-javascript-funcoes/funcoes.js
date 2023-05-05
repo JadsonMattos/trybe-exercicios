279fd16f84b80004289acb6acf4a17858a629af4
JadsonMattos <jadson.sousamattos@gmail.com>
2023-05-05 08:33:15 -0300

    Iniciando o entendimento acerca de funções

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-03-javascript-funcoes/funcoes.js
@@ -0,0 +1,14 @@
+// Considere a variável saldo, que representa a quantia em conta de determinada(o) cliente do TrybeBank e escreva quatro programas que:
+
+// Adicione um valor ao saldo.
+let balance = 19;
+function adicionaValorSaldo(valor) {};
+
+// Subtraia um valor do saldo.
+function subtraiSaldo(valor) {};
+
+// Multiplique o valor do saldo por uma taxa.
+function multiplicaSaldo(valor) {};
+
+// Divida o valor do saldo.
+function dividesaldo(valor) {};
