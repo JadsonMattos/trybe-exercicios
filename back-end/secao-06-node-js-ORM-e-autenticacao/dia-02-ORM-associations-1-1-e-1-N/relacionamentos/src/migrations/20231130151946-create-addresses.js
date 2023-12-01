f4201e5147128d67bd9bea0308173faf54bfd447
jadsonmattos <jadsonmattos@jadsonmattos.jadsonmattos>
2023-12-01 08:37:34 -0300

    exercícios 6.2

---
+++ back-end/secao-06-node-js-ORM-e-autenticacao/dia-02-ORM-associations-1-1-e-1-N/relacionamentos/src/migrations/20231130151946-create-addresses.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('addresses', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      employeeId: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'employee_id',
+        references: {
+          model: 'employees',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('addresses');
+  },
+};
