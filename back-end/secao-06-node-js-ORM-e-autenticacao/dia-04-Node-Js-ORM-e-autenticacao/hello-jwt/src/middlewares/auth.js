84fe1b3a2bcc59c785c2b2d811fddf8887b67ce6
jadsonmattos <jadsonmattos@jadsonmattos.jadsonmattos>
2023-12-05 10:22:00 -0300

    exercícios 6.4

---
+++ back-end/secao-06-node-js-ORM-e-autenticacao/dia-04-Node-Js-ORM-e-autenticacao/hello-jwt/src/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+function extractToken(bearerToken) {
+  return bearerToken.split(' ')[1];
+}
+
+module.exports = (req, res, next) => {
+  const bearerToken = req.header('Authorization');
+
+  if (!bearerToken) {
+    const err = new Error('Token not found');
+    err.statusCode = 401;
+    return next(err);
+  }
+
+  const token = extractToken(bearerToken);
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    req.user = payload;
+    return next();
+  } catch (err) {
+    err.statusCode = 401;
+    return next(err);
+  }
+};
